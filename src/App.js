b1d7caf955ee5b40d60205b8c8f9d14027baac22
Isaacjosh23 <isaacjoshua969@gmail.com>
2025-07-18 16:58:54 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
+  return (
+    <div className="counter">
+      <div className="range">
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+
+        <span>Step: {step}</span>
+      </div>
+
+      <div className="btn-con">
+        <button onClick={() => setCount((c) => c - step)}>&minus;</button>
+
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from now is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleReset();
+            }}
+            className="reset"
+          >
+            Reset
+          </button>
+        </div>
+      ) : (
+        <div style={{ height: "35px", padding: "10px 20px" }}></div>
+      )}
+    </div>
+  );
+}
